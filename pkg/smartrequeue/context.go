2edfb258722de3ed532aebc50a3a24c07e38386a
Maximilian Techritz <maximilian.techritz@sap.com>
2025-06-20 08:36:53 +0200

    docs: add more comments

--- pkg/smartrequeue/context.go
+++ pkg/smartrequeue/context.go
@@ -2,12 +2,15 @@ package smartrequeue
 
 import "context"
 
+// contextKey is a type used as a key for storing and retrieving the Entry from the context.
 type contextKey struct{}
 
+// NewContext creates a new context with the given Entry.
 func NewContext(ctx context.Context, entry *Entry) context.Context {
 	return context.WithValue(ctx, contextKey{}, entry)
 }
 
+// FromContext retrieves the Entry from the context, if it exists.
 func FromContext(ctx context.Context) *Entry {
 	s, ok := ctx.Value(contextKey{}).(*Entry)
 	if !ok {
