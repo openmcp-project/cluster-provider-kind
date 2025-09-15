edcd6b7028a25d9121caedd194d6f9d7ce1143c3
Valentin Gerlach <valentin.gerlach@sap.com>
2025-09-15 13:26:17 +0200

    feat: add script for local development (#70)
    * fix(deps): update module github.com/openmcp-project/openmcp-operator/api to v0.13.0
    
    * Run as root
    
    * Add script to set up a local dev environment
    
    * Use openmcp-operator v0.13.0
    
    * docs: add troubleshooting section
    
    * use shorter name for kind clusters
    
    * install a service provider and create an MCP
    
    * use correct prefix for subnet annotation
    
    * allow override of kind cluster name
    
    * create cluster resource for platform
    
    * use architecture of host
    
    * feat: release v0.0.15
    
    ---------
    
    Co-authored-by: René Schünemann <rene.schuenemann@sap.com>

---
+++ hack/local-dev.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+set -e -o pipefail
+
+# kind delete clusters --all
+# task build:img:build
+
+OPENMCP_OPERATOR_VERSION=v0.13.0
+OPENMCP_OPERATOR_IMAGE=ghcr.io/openmcp-project/images/openmcp-operator:${OPENMCP_OPERATOR_VERSION}
+
+OPENMCP_CP_KIND_VERSION=$(task version)-linux-$(go env GOARCH)
+OPENMCP_CP_KIND_IMAGE=ghcr.io/openmcp-project/images/cluster-provider-kind:${OPENMCP_CP_KIND_VERSION}
+
+OPENMCP_ENVIRONMENT=debug
+OPENMCP_PLATFORM_NAME=platform
+
+# Create platform cluster
+kind create cluster --name ${OPENMCP_PLATFORM_NAME} --config - << EOF
+apiVersion: kind.x-k8s.io/v1alpha4
+kind: Cluster
+nodes:
+- role: control-plane
+  extraMounts:
+  - hostPath: /var/run/docker.sock
+    containerPath: /var/run/host-docker.sock
+EOF
+
+# Pull images if needed
+docker image inspect ${OPENMCP_OPERATOR_IMAGE} || docker image pull ${OPENMCP_OPERATOR_IMAGE}
+docker image inspect ${OPENMCP_CP_KIND_IMAGE} || docker image pull ${OPENMCP_CP_KIND_IMAGE}
+
+# Load images
+kind load docker-image --name ${OPENMCP_PLATFORM_NAME} ${OPENMCP_OPERATOR_IMAGE}
+kind load docker-image --name ${OPENMCP_PLATFORM_NAME} ${OPENMCP_CP_KIND_IMAGE}
+
+# Create openmcp-system Namespace
+kubectl apply -f - << EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openmcp-system
+spec: {}
+EOF
+
+# Create openmcp-operator ServiceAccount
+kubectl apply -f - << EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: openmcp-operator
+  namespace: openmcp-system
+EOF
+
+# Create ClusterRoleBinding for openmcp-operator
+kubectl apply -f - << EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: openmcp-operator
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: openmcp-operator
+  namespace: openmcp-system
+EOF
+
+# Create ConfigMap for openmcp-operator
+kubectl apply -f - << EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: openmcp-operator
+  namespace: openmcp-system
+data:
+  config: |
+    managedControlPlane:
+      mcpClusterPurpose: mcp
+    scheduler:
+      scope: Cluster
+      purposeMappings:
+        mcp:
+          template:
+            spec:
+              profile: kind
+              tenancy: Exclusive
+        platform:
+          template:
+            spec:
+              profile: kind
+              tenancy: Shared
+        onboarding:
+          template:
+            spec:
+              profile: kind
+              tenancy: Shared
+EOF
+
+# Create openmcp-operator Deployment
+kubectl apply -f - << EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: openmcp-operator
+  namespace: openmcp-system
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: openmcp-operator
+  template:
+    metadata:
+      labels:
+        app: openmcp-operator
+    spec:
+      serviceAccountName: openmcp-operator
+      initContainers:
+      - image: ${OPENMCP_OPERATOR_IMAGE}
+        name: openmcp-operator-init
+        resources: {}
+        args:
+        - init
+        - --environment
+        - ${OPENMCP_ENVIRONMENT}
+        - --config
+        - /etc/openmcp-operator/config
+        env:
+        - name: POD_NAME
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: metadata.name
+        - name: POD_NAMESPACE
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: metadata.namespace
+        - name: POD_IP
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: status.podIP
+        - name: POD_SERVICE_ACCOUNT_NAME
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: spec.serviceAccountName
+        volumeMounts:
+        - name: config
+          mountPath: /etc/openmcp-operator
+          readOnly: true
+      containers:
+      - image: ${OPENMCP_OPERATOR_IMAGE}
+        name: openmcp-operator
+        resources: {}
+        args:
+        - run
+        - --environment
+        - ${OPENMCP_ENVIRONMENT}
+        - --config
+        - /etc/openmcp-operator/config
+        env:
+        - name: POD_NAME
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: metadata.name
+        - name: POD_NAMESPACE
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: metadata.namespace
+        - name: POD_IP
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: status.podIP
+        - name: POD_SERVICE_ACCOUNT_NAME
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: spec.serviceAccountName
+        volumeMounts:
+        - name: config
+          mountPath: /etc/openmcp-operator
+          readOnly: true
+      volumes:
+      - name: config
+        configMap:
+          name: openmcp-operator
+EOF
+
+# Wait for ClusterProvider CRD to be created
+echo Waiting for ClusterProvider CRD to be available...
+kubectl wait --for=create customresourcedefinitions.apiextensions.k8s.io/clusterproviders.openmcp.cloud --timeout=60s
+
+# Install ClusterProvider for kind
+kubectl apply -f - << EOF
+apiVersion: openmcp.cloud/v1alpha1
+kind: ClusterProvider
+metadata:
+  name: kind
+spec:
+  image: ${OPENMCP_CP_KIND_IMAGE}
+  extraVolumes:
+  - name: docker-socket
+    hostPath:
+      path: /var/run/host-docker.sock
+      type: Socket
+  extraVolumeMounts:
+  - name: docker-socket
+    mountPath: /var/run/docker.sock
+EOF
+
+# Create Cluster resource for platform
+kubectl apply -f - << EOF
+apiVersion: clusters.openmcp.cloud/v1alpha1
+kind: Cluster
+metadata:
+  annotations:
+    kind.clusters.openmcp.cloud/name: ${OPENMCP_PLATFORM_NAME}
+  name: platform
+  namespace: openmcp-system
+spec:
+  kubernetes: {}
+  profile: kind
+  purposes:
+  - platform
+  tenancy: Shared
+EOF
+
+# Install Service Provider Crossplane
+kubectl apply -f - << EOF
+apiVersion: openmcp.cloud/v1alpha1
+kind: ServiceProvider
+metadata:
+  name: crossplane
+spec:
+  image: ghcr.io/openmcp-project/images/service-provider-crossplane:v0.0.4
+EOF
+
+echo Waiting for the onboarding cluster to be created...
+kubectl wait --for=create -n openmcp-system cluster/onboarding --timeout=60s
+echo Waiting for the onboarding cluster to be ready...
+kubectl wait --for='jsonpath={.status.conditions[?(@.type=="Ready")].status}=True' -n openmcp-system cluster/onboarding --timeout=120s
+kind export kubeconfig --name $(kind get clusters | grep onboarding -m 1)
+
+# Install Service Provider Crossplane
+kubectl apply -f - << EOF
+apiVersion: core.openmcp.cloud/v2alpha1
+kind: ManagedControlPlaneV2
+metadata:
+  name: test
+spec:
+  iam: {}
+EOF
